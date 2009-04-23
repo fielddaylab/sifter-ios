cf1ac10de6f84668825695b0c79b250e56836049
mrdavidgagnon <mrdavidgagnon@gmail.com>
2009-04-23 18:48:05 +0000

    Game backup now dynamicly determines the tables to dump instead of relying on a hard coded list

--- editor/games_backup.php
+++ editor/games_backup.php
@@ -10,54 +10,90 @@
 
 	if (!isSet($_REQUEST['prefix'])) die ("<h3>You did not arrive at this page correctly</h3>");
 	$prefix = substr($_REQUEST['prefix'],0,strlen($_REQUEST['prefix'])-1);
+
+	$tmpDir = "{$prefix}_backup_" . date('Y_m_d');
+	
+	
+	//Delete a previous backup with the same name
+	$rmCommand = "rm -rf {$engine_sites_path}/Backups/{$tmpDir}";
+	echo "<p>Running: $rmCommand </p>";
+	exec($rmCommand, $output, $return);
+	if ($return) die ("<h3>There was an error cleaning up: $return </h3>
+					  <p>Check your file permissions<p>");
+	else echo "<p>Cleaned up</p>";
 	
 	//Set up a tmp directory
-	$tmpDir = "{$prefix}_backup_" . date('Y_m_d');
-	exec("mkdir {$engine_sites_path}/Backups/{$tmpDir}", $output, $return);
-	if ($return) die ("<h3>There was an error creating the directory {$engine_sites_path}/Backups/{$tmpDir}</h3>
+	$mkdirCommand = "mkdir {$engine_sites_path}/Backups/{$tmpDir}";
+	echo "<p>Running: $mkdirCommand </p>";
+	exec($mkdirCommand, $output, $return);
+	if ($return) die ("<h3>There was an error creating the directory {$engine_sites_path}/Backups/{$tmpDir}: $return </h3>
 					  <p>Check your config file and your permissions on {$engine_sites_path}<p>");
 	
 	//Create SQL File
 	$sqlFile = 'database.sql';
-	$tables = "{$prefix}_applications {$prefix}_events {$prefix}_items {$prefix}_locations {$prefix}_log {$prefix}_nodes {$prefix}_npcs {$prefix}_npc_conversations {$prefix}_player_applications {$prefix}_player_events {$prefix}_player_items";
+	
+	$getTablesCommand = "mysql --user={$opts['un']} --password={$opts['pw']} -B --skip-column-names INFORMATION_SCHEMA -e \"SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA='{$opts['db']}' AND TABLE_NAME LIKE '{$prefix}_%'\"";
+	echo "<p>Running: $getTablesCommand </p>";
+	
+	exec($getTablesCommand, $output, $return);
+	
+	var_dump($output);
+	$tables = '';
+	foreach ($output as $table) {
+		$tables .= $table;
+		$tables .= ' ';
+	}
+	
 	$createSQLCommand = "{$mysql_bin_path}/mysqldump -u {$opts['un']} --password={$opts['pw']} {$opts['db']} $tables > {$engine_sites_path}/Backups/{$tmpDir}/{$sqlFile}";
-	//echo $createSQLCommand;
+	echo "<p>Running: $createSQLCommand </p>";
 	exec($createSQLCommand, $output, $return);
-	if ($return) die ("<h3>There was an error exporting the SQL file</h3>
+	if ($return) die ("<h3>There was an error exporting the SQL file: {$output['0']} </h3>
 					  <p>Check your config file for the mysql bin path and username / password<p>");
 	else echo "<p>SQL Data Dumped</p>";
 	
+	
 	//Copy the site into the tmp directory
-	$copyCommand = "cp /{$engine_sites_path}/{$prefix}.php {$engine_sites_path}/Backups/{$tmpDir}";
+	$copyCommand = "cp {$engine_sites_path}/{$prefix}.php {$engine_sites_path}/Backups/{$tmpDir}";
+	echo "Running: $copyCommand </p>";
 	exec($copyCommand, $output, $return);
-	if ($return) die ("<h3>There was an error copying your files to the temp directory</h3>
+	if ($return) die ("<h3>There was an error copying your files to the temp directory: $return </h3>
 					  <p>Check your config file and file permissions<p>");
 	else echo "<p>Game Files Dumped</p>";	
-	$copyCommand = "cp -R /{$engine_sites_path}/{$prefix} {$engine_sites_path}/Backups/{$tmpDir}/{$prefix}";
+	
+	$copyCommand = "cp -R {$engine_sites_path}/{$prefix} {$engine_sites_path}/Backups/{$tmpDir}/{$prefix}";
+	echo "<p>Running: $copyCommand </p>";
 	exec($copyCommand, $output, $return);
-	if ($return) die ("<h3>There was an error copying your files to the temp directory</h3>
+	if ($return) die ("<h3>There was an error copying your files to the temp directory: $return </h3>
 					  <p>Check your config file and file permissions<p>");
 	else echo "<p>Game PHP file dumped</p>";
 	
+	
 	//Create a version file
 	$versionCommand = "{$svn_bin_path}/svnversion {$engine_path} > {$engine_sites_path}/Backups/{$tmpDir}/version";
+	echo "<p>Running: $versionCommand </p>";
 	exec($versionCommand, $output, $return);
-	if ($return) echo ("<h3>There was an error creating a version file</h3>
+	if ($return) echo ("<h3>There was an error creating a version file: $return </h3>
 					  <p>Check your svn_bin_path in the config file<p>");
 	else echo "<p>Version Recorded</p>";
 	
+	
 	//Zip up the whole directory
 	$zipFile = "{$prefix}_backup_" . date('Y_m_d') . ".tar";
-	chdir("/{$engine_sites_path}/Backups");
+	$newWd = "{$engine_sites_path}/Backups";
+	echo "<p>Changing Directory to $newWd </p>";
+	chdir($newWd);
 	$createZipCommand = "tar -cf {$engine_sites_path}/Backups/{$zipFile} {$tmpDir}/";
+	echo "<p>Running: $createZipCommand </p>";
 	exec($createZipCommand, $output, $return);
-	if ($return) die ("<h3>There was an error compressing your files</h3>
+	if ($return) die ("<h3>There was an error compressing your files: $return </h3>
 					   <p>Check that tar is installed an in path<p>");
 	else echo "<p>Compressed Files</p>";
 	
-	$rmCommand = "rm -rf /{$engine_sites_path}/Backups/{$tmpDir}";
+	//Delete the Temp
+	$rmCommand = "rm -rf {$engine_sites_path}/Backups/{$tmpDir}";
+	echo "<p>Running: $rmCommand </p>";
 	exec($rmCommand, $output, $return);
-	if ($return) die ("<h3>There was an error cleaning up</h3>
+	if ($return) die ("<h3>There was an error cleaning up: $return </h3>
 					   <p>Check your file permissions<p>");
 	else echo "<p>Cleaned up</p>";
 	
