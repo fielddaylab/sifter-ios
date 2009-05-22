8812d8228a1f64a90b73e7eb5e044adc3750828a
mrdavidgagnon <mrdavidgagnon@gmail.com>
2009-05-22 16:02:57 +0000

    Reverted to pre-nodes-conversations-npcs branch

--- engine/Framework/Controller/SimpleREST.php
+++ engine/Framework/Controller/SimpleREST.php
@@ -178,9 +178,7 @@ class Framework_Controller_SimpleREST extends Framework_Controller_Web
                         }
                         else {
                             // It's safe to add them as attributes
-                            $groupChild = array_key_exists('xmlType', $childV)
-                                ? $child->addChild($childV['xmlType'])
-                                : $child->addChild('row');
+                            $groupChild = $child->addChild('row');
                             foreach ($childV as $testK => $testV) {
                                 $groupChild->addAttribute($testK,
                                     $testV);
