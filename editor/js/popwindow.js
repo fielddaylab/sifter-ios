710d82d6117ddd032d0b987f28ea060023db3b23
mrdavidgagnon <mrdavidgagnon@gmail.com>
2010-03-12 17:59:14 +0000

    clearing old trunk

--- editor/js/popwindow.js
+++
@@ -1,6 +0,0 @@
-<!--
-function popWindow(url, width, height) {
-  var Win = window.open(url,"popWindow",'width=' + width + ',height=' + height + ',resizable=1,scrollbars=yes,menubar=no,status=no' );
-}
-//-->
-
--- editor/triggers/refresh.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<script type='text/javascript'>
-		window.location='{$_SERVER['PHP_SELF']}';
-	</script>";
-?>
